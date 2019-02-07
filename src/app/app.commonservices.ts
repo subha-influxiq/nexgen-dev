57a0e7a8ec11fe7ad21ffdba1f1e4767d0f96445
ipsitainfluxiq <ipsita.influxiq@gmail.com>
2019-02-07 16:28:11 +0530

    7/2/19-Ipsita

--- src/app/app.commonservices.ts
+++ src/app/app.commonservices.ts
@@ -6,40 +6,24 @@ import {Injectable} from '@angular/core';
 import {  HttpClient } from '@angular/common/http';
 @Injectable()
 export class Commonservices {
-    items: Array<any>;
+  /*  items: Array<any>;*/
     url: any;
-    uploadurl: any;
+  /*  uploadurl: any;
     fileurl: any;
     public filedeleteurl: string;
     public verifyurl: any;
     public filepathurl: string;
-    public base64encode: any;
+    public base64encode: any;*/
     public nodesslurl: any;
 
     constructor(private http: HttpClient) {
-    /*    this.url = 'http://shattarblock.influxiq.com/php/server.php?q=';
-        this.verifyurl = 'http://shattarblock.influxiq.com/php/smsverification.php';
-        this.uploadurl = 'http://shattarblock.influxiq.com/php/fileupload.php';
-        this.filepathurl = 'http://shattarblock.influxiq.com/nodeserver/uploads/';
-        this.filedeleteurl = 'http://shattarblock.influxiq.com/php/scrappage.php';
-        this.fileurl = 'https://shattarblock.influxiq.com/php/uploads/';
-        this.base64encode = 'https://shattarblock.influxiq.com/php/index.php?encode=encodefile';*/
-
-        this.uploadurl = 'http://shattarblock.influxiq.com/php/index.php';
+       /* this.uploadurl = 'http://shattarblock.influxiq.com/php/index.php';
         this.filedeleteurl = 'http://shattarblock.influxiq.com/php/scrappage.php';
         this.fileurl = 'https://shattarblock.influxiq.com/php/uploads/';
         this.filepathurl = 'https://shattarblock.influxiq.com/php/uploads/';
-        this.base64encode = 'https://shattarblock.influxiq.com/php/index.php?encode=encodefile';
+        this.base64encode = 'https://shattarblock.influxiq.com/php/index.php?encode=encodefile';*/
         this.url = 'https://shattarblock.influxiq.com/php/index.php?q=';
-        this.nodesslurl = 'https://nodessl.influxiq.com:6021/';
-
-        /*  if (window.location.hostname == 'localhost') {
-         this.url = 'http://localhost:3000/';
-         } else {
-         //  this.url = 'http://influxiq.com:3014/';
-         this.url = 'http://geofencedsp.com:3000/';
-         }*/
-
+        this.nodesslurl = 'https://nodessl.influxiq.com:6022/';
     }
     shorten(str, maxLen, separator = '') {
         if (str.length <= maxLen) return str;
