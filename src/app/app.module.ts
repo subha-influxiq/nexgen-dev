b0a9dc991417ace38d0d20de8e4c71ad4bd7d255
ipsitainfluxiq <ipsita.influxiq@gmail.com>
2019-02-07 16:34:45 +0530

    7/2/19-Ipsita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { FunnelComponent } from './funnel/funnel.component';
 import { SignupComponent } from './signup/signup.component';
 import {appRoutingProviders, routing} from './route';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     FunnelComponent,
-    SignupComponent
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    routing
+    HttpClientModule,
+    routing,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
