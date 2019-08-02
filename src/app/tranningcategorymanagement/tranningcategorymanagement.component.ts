1a9e227c736bb9fb849fa9cf5738c2cc925e4c1e
Subha <subha.influxiq@gmail.com>
2019-08-02 18:52:05 +0530

    update listing order

--- src/app/tranningcategorymanagement/tranningcategorymanagement.component.ts
+++ src/app/tranningcategorymanagement/tranningcategorymanagement.component.ts
@@ -17,6 +17,7 @@ public sourcecondition:any={};
     {value:'id',name:'Id',role:0,func:'',class:'id',type:'#'},
     {value:'categoryname',name:'Training Title',role:0,func:'',class:'categoryname',type:'text'},
     {value:'description',name:'Description',role:0,func:'',class:'description',type:'text'},
+    {value:'priority',name:'Priority',role:0,func:'',class:'priority',type:'text'},
     {value:'type1',name:'Type',role:0,func:'',class:'type',type:'text'},
     // {value:'roleaccess',name:'Role Access',role:0,func:'',class:'roleaccess',type:'text'},
     {value:'status',name:'Status',role:0,func:'',class:'status',type:'checkbox',editrole:['admin']},
@@ -24,6 +25,7 @@ public sourcecondition:any={};
   this.formdata=[
     {inputtype:'text',name:'categoryname',label:'Training Title',placeholder:'Enter Training Title',validationrule:{required:true},validationerrormsg:'is required'},
     {inputtype:'textarea',name:'description',label:'Description',placeholder:'Enter Description',validationrule:{required:true},validationerrormsg:'is required'},
+    {inputtype:'text',name:'priority',label:'Priority',placeholder:'Enter Priority',validationrule:{required:true},validationerrormsg:'is required'},
     {inputtype:'select',name:'type1',label:'Training Type',defaultchoice:'Select Training',sourceview:'tranning',sourcetype:'static',selectvalue:'name',selectid:'name',validationrule:{required:true},validationerrormsg:'is required'},
 
     // {inputtype:'select',name:'roleaccess',label:'Roles can Access',defaultchoice:'Select Roles',sourceview:'roles',sourcetype:'static',selectvalue:'name',selectid:'id',validationrule:{required:true},validationerrormsg:'is required',multiple:true},
