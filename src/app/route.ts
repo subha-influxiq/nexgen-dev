3995e9a8a94a5066441a56dc90552a98919be6e0
root <chandrani.influxiq@gmail.com>
2019-08-06 16:08:08 +0530

    added module named training report;changes in regionaldashboard,dashboard and rep list

--- src/app/route.ts
+++ src/app/route.ts
@@ -5,51 +5,51 @@
 
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LoginComponent} from "./login/login.component";
-import {FunnelComponent} from "./funnel/funnel.component";
-import {DashboardComponent} from "./dashboard/dashboard.component";
-import {AdminmanagementComponent} from "./adminmanagement/adminmanagement.component";
-import {RegionalRecruiterComponent} from "./regional-recruiter/regional-recruiter.component";
-import {RepComponent} from "./rep/rep.component";
-import {RegionalDashboardComponent} from "./regional-dashboard/regional-dashboard.component";
-import {RepDashboardComponent} from "./rep-dashboard/rep-dashboard.component";
-import {SignupComponent} from "./signup/signup.component";
-import {HomeComponent} from "./home/home.component";
-import {ContractComponent} from "./contract/contract.component";
-import {TranningcategorymanagementComponent} from "./tranningcategorymanagement/tranningcategorymanagement.component";
-import {UseraccountsettingComponent} from "./useraccountsetting/useraccountsetting.component";
-import {RepresentativelistComponent} from "./representativelist/representativelist.component";
-import {RepTraingcenterComponent} from "./rep-traingcenter/rep-traingcenter.component";
-import {TrialsComponent} from "./trials/trials.component";
-import {TrainingsectionComponent} from "./trainingsection/trainingsection.component";
-import {TrainingsectionlistComponent} from "./trainingsectionlist/trainingsectionlist.component";
-import {FrontendheaderComponent} from "./frontendheader/frontendheader.component";
-import {FrontendfooterComponent} from "./frontendfooter/frontendfooter.component";
-import {FrontendhomeComponent} from "./frontendhome/frontendhome.component";
+import { LoginComponent } from "./login/login.component";
+import { FunnelComponent } from "./funnel/funnel.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { AdminmanagementComponent } from "./adminmanagement/adminmanagement.component";
+import { RegionalRecruiterComponent } from "./regional-recruiter/regional-recruiter.component";
+import { RepComponent } from "./rep/rep.component";
+import { RegionalDashboardComponent } from "./regional-dashboard/regional-dashboard.component";
+import { RepDashboardComponent } from "./rep-dashboard/rep-dashboard.component";
+import { SignupComponent } from "./signup/signup.component";
+import { HomeComponent } from "./home/home.component";
+import { ContractComponent } from "./contract/contract.component";
+import { TranningcategorymanagementComponent } from "./tranningcategorymanagement/tranningcategorymanagement.component";
+import { UseraccountsettingComponent } from "./useraccountsetting/useraccountsetting.component";
+import { RepresentativelistComponent } from "./representativelist/representativelist.component";
+import { RepTraingcenterComponent } from "./rep-traingcenter/rep-traingcenter.component";
+import { TrialsComponent } from "./trials/trials.component";
+import { TrainingsectionComponent } from "./trainingsection/trainingsection.component";
+import { TrainingsectionlistComponent } from "./trainingsectionlist/trainingsectionlist.component";
+import { FrontendheaderComponent } from "./frontendheader/frontendheader.component";
+import { FrontendfooterComponent } from "./frontendfooter/frontendfooter.component";
+import { FrontendhomeComponent } from "./frontendhome/frontendhome.component";
 
-import {WhoWeAreComponent} from "./who-we-are/who-we-are.component";
-import {AboutPcrTestingComponent} from "./about-pcr-testing/about-pcr-testing.component";
-import {GetStartedComponent} from "./get-started/get-started.component";
-import {ContactUsComponent} from "./contact-us/contact-us.component";
-import {RepdetailsComponent} from "./repdetails/repdetails.component";
-import {ReplegaldocumentComponent} from "./replegaldocument/replegaldocument.component";
-import {LegaldoclistComponent} from "./legaldoclist/legaldoclist.component";
-import {DigitalcontractComponent} from "./digitalcontract/digitalcontract.component";
-import {UsermanagementComponent} from "./usermanagement/usermanagement.component";
-import {TrainingcenterreoprtComponent} from "./trainingcenterreoprt/trainingcenterreoprt.component";
-import {EventmanagementComponent} from "./eventmanagement/eventmanagement.component";
-import {RepeventlistComponent} from "./repeventlist/repeventlist.component";
-import {ResourcecategoryComponent} from "./resourcecategory/resourcecategory.component";
-import {ResourcesComponent} from "./resources/resources.component";
-import {MyresourceComponent} from "./myresource/myresource.component";
-import {ManagequizComponent} from "./managequiz/managequiz.component";
+import { WhoWeAreComponent } from "./who-we-are/who-we-are.component";
+import { AboutPcrTestingComponent } from "./about-pcr-testing/about-pcr-testing.component";
+import { GetStartedComponent } from "./get-started/get-started.component";
+import { ContactUsComponent } from "./contact-us/contact-us.component";
+import { RepdetailsComponent } from "./repdetails/repdetails.component";
+import { ReplegaldocumentComponent } from "./replegaldocument/replegaldocument.component";
+import { LegaldoclistComponent } from "./legaldoclist/legaldoclist.component";
+import { DigitalcontractComponent } from "./digitalcontract/digitalcontract.component";
+import { UsermanagementComponent } from "./usermanagement/usermanagement.component";
+import { TrainingcenterreoprtComponent } from "./trainingcenterreoprt/trainingcenterreoprt.component";
+import { EventmanagementComponent } from "./eventmanagement/eventmanagement.component";
+import { RepeventlistComponent } from "./repeventlist/repeventlist.component";
+import { ResourcecategoryComponent } from "./resourcecategory/resourcecategory.component";
+import { ResourcesComponent } from "./resources/resources.component";
+import { MyresourceComponent } from "./myresource/myresource.component";
+import { ManagequizComponent } from "./managequiz/managequiz.component";
 import { TestresolveService } from './testresolve.service';
-import {TempaccessComponent} from "./tempaccess/tempaccess.component";
-import {TestComponent} from "./test/test.component";
-import {ForgetpasswordComponent} from "./forgetpassword/forgetpassword.component";
-import {SlotviewComponent} from "./slotview/slotview.component";
-import {AppointmentlistComponent} from "./appointmentlist/appointmentlist.component";
-import {ResetpasswordComponent} from "./resetpassword/resetpassword.component";
+import { TempaccessComponent } from "./tempaccess/tempaccess.component";
+import { TestComponent } from "./test/test.component";
+import { ForgetpasswordComponent } from "./forgetpassword/forgetpassword.component";
+import { SlotviewComponent } from "./slotview/slotview.component";
+import { AppointmentlistComponent } from "./appointmentlist/appointmentlist.component";
+import { ResetpasswordComponent } from "./resetpassword/resetpassword.component";
 
 /* 11th July 2019 */
 import { AddEditComponent } from "./tranningcategorymanagement/add-edit/add-edit.component";
@@ -58,81 +58,84 @@ import { AgreementComponent } from "./agreement/agreement.component";
 /* added by Chandrani */
 import { ManageleadsComponent } from "./manageleads/manageleads.component";
 import { ProductsComponent } from "./products/products.component";
+import { TrainingreportsComponent } from "./trainingreports/trainingreports.component";
 
 const appRoutes: Routes = [
-    { path: '', redirectTo:'/login', pathMatch: 'full' },
-    { path: 'login', component:  LoginComponent},
-    { path: 'funnel', component: FunnelComponent},
-    { path: 'dashboard', component: DashboardComponent, resolve: {results: TestresolveService},data: { link: 'datalist',source:'users',condition:{type:'rep'} }},
- //   { path: 'dashboard', component: DashboardComponent},
-    { path: 'admin', component: AdminmanagementComponent},
-    { path: 'regional', component: RegionalRecruiterComponent},
-    { path: 'rep', component: RepComponent},
-    { path: 'regionaldashboard', component: RegionalDashboardComponent},
-    { path: 'repdashboard', component: RepDashboardComponent},
-    { path: 'signup/:id', component: SignupComponent},
-   /* { path: '', component: HomeComponent},*/
-    { path: 'contract', component: ContractComponent},
-    {path: 'regionaldashboard', component: RegionalRecruiterComponent},
-    {path: 'tranningcategory', component: TranningcategorymanagementComponent},
-    {path: 'addtrainings', component: TranningcategorymanagementComponent},
-    { path:'useraccountsetting', component: UseraccountsettingComponent},
-    { path:'useraccountsetting/:id', component: UseraccountsettingComponent},
-    { path: 'representativelist', component: RepresentativelistComponent},
-    { path: 'reptrainingcenter', component: RepTraingcenterComponent},
-    { path: 'reptrainingcenter/:cid', component: RepTraingcenterComponent}, // cat id
-    { path: 'reptrainingcenter/:cid/:lid', component: RepTraingcenterComponent}, // lesson id
-    { path:'trial', component: TrialsComponent},
-    { path:'trainingsectionlist', component: TrainingsectionlistComponent},
-    { path:'trainingsection', component: TrainingsectionComponent},
-    { path:'trainingsection/:id', component: TrainingsectionComponent},
-    { path:'frontendheader', component: FrontendheaderComponent},
-    { path:'frontendfooter', component: FrontendfooterComponent},
-    { path:'', component: FrontendhomeComponent},
-    { path:'who_we_are', component: WhoWeAreComponent},
-    { path:'about_pcr_testing', component: AboutPcrTestingComponent},
-    { path:'get_started', component: GetStartedComponent},
-    { path:'contact_us', component: ContactUsComponent},
-    { path:'repdetails/:id', component: RepdetailsComponent},
-    { path:'replegaldocuments', component: ReplegaldocumentComponent},
-    { path:'legaldoclist', component: LegaldoclistComponent},
-    { path:'digitalcontract', component: DigitalcontractComponent},
-    { path:'usermanagement', component: UsermanagementComponent},
-    { path:'trainingreport', component: TrainingcenterreoprtComponent},
-    { path:'event', component: EventmanagementComponent},
-    { path:'event/:at/:val/:rt', component: EventmanagementComponent},
-    { path:'calendar', component: EventmanagementComponent},
-    { path:'calendar/:at/:val/:rt', component: EventmanagementComponent},
-    { path:'repevent/:id', component: RepeventlistComponent},
-    { path:'resourcecategory', component: ResourcecategoryComponent},
-    { path:'resources', component: ResourcesComponent},
-    { path:'myresource', component: MyresourceComponent},
-    { path:'myresource/:catid', component: MyresourceComponent},
-    { path:'managequiz/:lessonid', component: ManagequizComponent},
-    { path: 'tempaccess', component: TempaccessComponent},
-    { path: 'on-boarding-call-booked/:id', component: TempaccessComponent},
-    { path: 'test', component: TestComponent},
-    { path: 'forgetpassword', component: ForgetpasswordComponent},
-    { path: 'slotview', component: SlotviewComponent},
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'funnel', component: FunnelComponent },
+    { path: 'dashboard', component: DashboardComponent, resolve: { results: TestresolveService }, data: { link: 'datalist', source: 'users', condition: { type: 'rep' } } },
+    //   { path: 'dashboard', component: DashboardComponent},
+    { path: 'admin', component: AdminmanagementComponent },
+    { path: 'regional', component: RegionalRecruiterComponent },
+    { path: 'rep', component: RepComponent },
+    { path: 'regionaldashboard', component: RegionalDashboardComponent },
+    { path: 'repdashboard', component: RepDashboardComponent },
+    { path: 'signup/:id', component: SignupComponent },
+    /* { path: '', component: HomeComponent},*/
+    { path: 'contract', component: ContractComponent },
+    { path: 'regionaldashboard', component: RegionalRecruiterComponent },
+    { path: 'tranningcategory', component: TranningcategorymanagementComponent },
+    { path: 'addtrainings', component: TranningcategorymanagementComponent },
+    { path: 'useraccountsetting', component: UseraccountsettingComponent },
+    { path: 'useraccountsetting/:id', component: UseraccountsettingComponent },
+    { path: 'representativelist', component: RepresentativelistComponent },
+    { path: 'reptrainingcenter', component: RepTraingcenterComponent },
+    { path: 'reptrainingcenter/:cid', component: RepTraingcenterComponent }, // cat id
+    { path: 'reptrainingcenter/:cid/:lid', component: RepTraingcenterComponent }, // lesson id
+    { path: 'trial', component: TrialsComponent },
+    { path: 'trainingsectionlist', component: TrainingsectionlistComponent },
+    { path: 'trainingsection', component: TrainingsectionComponent },
+    { path: 'trainingsection/:id', component: TrainingsectionComponent },
+    { path: 'frontendheader', component: FrontendheaderComponent },
+    { path: 'frontendfooter', component: FrontendfooterComponent },
+    { path: '', component: FrontendhomeComponent },
+    { path: 'who_we_are', component: WhoWeAreComponent },
+    { path: 'about_pcr_testing', component: AboutPcrTestingComponent },
+    { path: 'get_started', component: GetStartedComponent },
+    { path: 'contact_us', component: ContactUsComponent },
+    { path: 'repdetails/:id', component: RepdetailsComponent },
+    { path: 'replegaldocuments', component: ReplegaldocumentComponent },
+    { path: 'legaldoclist', component: LegaldoclistComponent },
+    { path: 'digitalcontract', component: DigitalcontractComponent },
+    { path: 'usermanagement', component: UsermanagementComponent },
+    { path: 'trainingreport', component: TrainingcenterreoprtComponent },
+    { path: 'event', component: EventmanagementComponent },
+    { path: 'event/:at/:val/:rt', component: EventmanagementComponent },
+    { path: 'calendar', component: EventmanagementComponent },
+    { path: 'calendar/:at/:val/:rt', component: EventmanagementComponent },
+    { path: 'repevent/:id', component: RepeventlistComponent },
+    { path: 'resourcecategory', component: ResourcecategoryComponent },
+    { path: 'resources', component: ResourcesComponent },
+    { path: 'myresource', component: MyresourceComponent },
+    { path: 'myresource/:catid', component: MyresourceComponent },
+    { path: 'managequiz/:lessonid', component: ManagequizComponent },
+    { path: 'tempaccess', component: TempaccessComponent },
+    { path: 'on-boarding-call-booked/:id', component: TempaccessComponent },
+    { path: 'test', component: TestComponent },
+    { path: 'forgetpassword', component: ForgetpasswordComponent },
+    { path: 'slotview', component: SlotviewComponent },
 
-    { path: 'on-boarding-call/:id', component: SlotviewComponent},
-    { path: 'discovery-call/', component: SlotviewComponent},
+    { path: 'on-boarding-call/:id', component: SlotviewComponent },
+    { path: 'discovery-call/', component: SlotviewComponent },
 
-    { path: 'slotview/:id', component: SlotviewComponent},
-    { path: 'appointmentlist', component: AppointmentlistComponent},
-    { path: 'resetpassword/:id', component: ResetpasswordComponent},
+    { path: 'slotview/:id', component: SlotviewComponent },
+    { path: 'appointmentlist', component: AppointmentlistComponent },
+    { path: 'resetpassword/:id', component: ResetpasswordComponent },
 
     /* changes 11th July 2019 */
     { path: 'category-management/add-new', component: AddEditComponent },
     { path: 'category-management/edit/:id', component: AddEditComponent },
-    { path: 'agreement', component: AgreementComponent},
+    { path: 'agreement', component: AgreementComponent },
 
-    { path: 'manage-leads', component: ManageleadsComponent},
-    { path: 'products', component: ProductsComponent},
-    
+    /* added by Chandrani */
+    { path: 'manage-leads', component: ManageleadsComponent },
+    { path: 'products', component: ProductsComponent },
+    { path: 'training-reports', component: TrainingreportsComponent }
+
 ];
 
 export const appRoutingProviders: any[] = [
 ];
-export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes,{ useHash: false });
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes, { useHash: false });
 
---
+++ src/app/trainingreports/trainingreports.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import {Commonservices} from '../app.commonservices' ;
+import { HttpClient } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+
+@Component({
+  selector: 'app-trainingreports',
+  templateUrl: './trainingreports.component.html',
+  styleUrls: ['./trainingreports.component.css'],
+  providers: [Commonservices]
+})
+export class TrainingreportsComponent implements OnInit {
+  public trainingReportArr:any=[];
+  public formdata: any;
+  public datasource: any;
+  public tabledatalist: any[];
+  public sourcecondition: any = {};
+  public hideaddval: any = true;
+  public hideactionval: any = true;
+
+  constructor(public router:Router,public http:HttpClient,public cookieservice:CookieService,public commonservices:Commonservices, private route: ActivatedRoute) {
+    this.tabledatalist = [
+      { value: 'id', name: 'Id', role: 0, func: '', class: 'id', type: '#' },
+      { value: 'fullname', name: 'Name', role: 0, func: '', class: 'fullname', type: 'text' },
+      { value: 'email', name: 'Email Id', role: 0, func: '', class: 'email', type: 'text' },
+      { value: 'reactsum', name: 'Training Completed (%)', role: 0, func: '', class: 'reactsum', type: 'text' },  
+      {value:'status',name:'Status',role:0,func:'',class:'status',type:'checkbox',editrole:['admin']},
+    ];
+    this.formdata = [
+      { inputtype: 'text', name: 'fullname', label: 'Full Name', placeholder: 'Enter Full Name', validationrule: { required: true }, validationerrormsg: 'is required' },
+      { inputtype: 'email', name: 'email', label: 'Email Id', placeholder: 'Enter Your Email', validationrule: { required: true, email: true }, validationerrormsg: 'is required and should be valid' },
+      { inputtype: 'hidden', name: 'created_by', label: "created_by", placeholder: "Created By", value: this.cookieservice.get('userid') },
+      {inputtype:'checkbox',name:'status',label:'Status',value:false},
+    ];
+    this.datasource = { table: 'users', objarr: ["created_by"] };
+
+   }
+
+  ngOnInit() {
+  }
+  
+
+}
