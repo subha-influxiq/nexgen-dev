d8ef82c76b9910cbba50669d8ab42601806dca05
root <chandrani.influxiq@gmail.com>
2019-08-08 16:35:04 +0530

    changes in manage leads

--- src/app/notelist/notelist.component.ts
+++ src/app/notelist/notelist.component.ts
@@ -47,8 +47,9 @@ export class NotelistComponent implements OnInit {
     });
     console.log(this.cookeiservice.get('usertype'));
     console.log(this.router.url);
+    
     if(this.router.url=='/manage-leads'){
-      this.sourcecondition = {'created_under_object':this.leaddata.created_by};
+      this.sourcecondition = {'created_under_object':this.leaddata._id};
     }
     if(this.router.url=='/appointmentlist'){
     this.sourcecondition = {'created_under_object':this.leaddata.userdata._id};
@@ -84,13 +85,13 @@ export class NotelistComponent implements OnInit {
       let link = this._commonservice.nodesslurl+'addorupdatedata';
       let data2 = this.noteForm.value;
       data2.created_by = this.cookeiservice.get('userid');
-      // data2.created_under = this.leaddata._id;
-      if(this.router.url=='/manage-leads'){
-        data2.created_under = this.leaddata.created_by;
-      }
-      if(this.router.url=='/appointmentlist'){
-        data2.created_under = this.leaddata.userdata._id;
-    }
+      data2.created_under = this.leaddata._id;
+    //   if(this.router.url=='/manage-leads'){
+    //     data2.created_under = this.leaddata.created_by;
+    //   }
+    //   if(this.router.url=='/appointmentlist'){
+    //     data2.created_under = this.leaddata.userdata._id;
+    // }
       let postdata:any;
       
       
@@ -99,8 +100,9 @@ export class NotelistComponent implements OnInit {
         postdata = { source: 'notes', data: data2, sourceobj: ["created_by","created_under"] };
       }else{
         console.log('null');
+        
         if(this.router.url=='/manage-leads'){
-          postdata = { source: 'notes', data: {note:this.noteForm.controls['note'].value,created_by:this.cookeiservice.get('userid'),created_under:this.leaddata.created_by}, sourceobj: ["created_by","created_under"] };
+          postdata = { source: 'notes', data: {note:this.noteForm.controls['note'].value,created_by:this.cookeiservice.get('userid'),created_under:this.leaddata._id}, sourceobj: ["created_by","created_under"] };
         }
         if(this.router.url=='/appointmentlist'){
           postdata = { source: 'notes', data: {note:this.noteForm.controls['note'].value,created_by:this.cookeiservice.get('userid'),created_under:this.leaddata.userdata._id}, sourceobj: ["created_by","created_under"] };
