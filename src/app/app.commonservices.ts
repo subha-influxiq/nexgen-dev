7b5bfc5e03fd919a1a894b39a962494718faf8c9
amitavainfluxiq <amitava.involutiontech@gmail.com>
2019-11-04 14:46:39 +0530

    by amitava

--- src/app/app.commonservices.ts
+++ src/app/app.commonservices.ts
@@ -133,8 +133,8 @@ export class Commonservices {
         return moment.unix(dt).format(" HH:mm A");
     }
     gettype(filename){
-        console.log('filename');
-        console.log(filename);
+        // console.log('filename');
+        // console.log(filename);
         //if(filename=='') return "DOC";
        var last;
         last = filename.substring(filename.lastIndexOf(".") + 1, filename.length);
