137a98ecfaab146091de8b52289077f7da15dd7f
koushikbiswas <biswakoushik.influxiq@gmail.com>
2020-02-21 19:19:26 +0530

    crm list page design and html

--- src/app/crm-lead-lists/crm-lead-lists.component.ts
+++ src/app/crm-lead-lists/crm-lead-lists.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-crm-lead-lists',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrmLeadListsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
+  }
+
+
+  leaddetailurl(val:any){
+    this.router.navigateByUrl('/crm-lead-details');
   }
 
 }
