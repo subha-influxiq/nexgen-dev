bdc19334a53026fa31d01bbbbb68626d77a6c57d
koushikbiswas <biswakoushik.influxiq@gmail.com>
2020-02-22 15:32:13 +0530

    CRM LEAD LIST AND DETAIL

--- src/app/crm-lead-details/crm-lead-details.component.ts
+++ src/app/crm-lead-details/crm-lead-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-crm-lead-details',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crm-lead-details.component.css']
 })
 export class CrmLeadDetailsComponent implements OnInit {
+  showMyInfoUpdateBox: boolean = false;
+  showUpdateNotes: boolean = false;
+  showSaveNote: boolean = false;
+  showEditNote: boolean = false;
+  public expandedIndex:any;
+  public ActionLists:any;
+  public subActionLists:any;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.expandedIndex = -1  
+    this.ActionLists = [{name:"Status"},{name:"Hipster Ipsum"},{name:"Corporate Ipsum"},{name:"Legal Ipsum"}]
   }
+
+  Collaps(index: number) {  
+    this.expandedIndex = index === this.expandedIndex ? -1 : index;
+    //some values  
+    this.subActionLists = [{subitem:"subth1"},{subitem:"subth2"},{subitem:"subth3"}];    
+  }     
 
 }
